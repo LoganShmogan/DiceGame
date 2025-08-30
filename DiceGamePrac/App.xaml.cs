5315aba644c8c3b0973b5f8a1d30e531c88b52ea
Logan Young <younglogan09@gmail.com>
2025-08-31 11:48:59 +1200

    First Commit

---
+++ DiceGamePrac/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace DiceGamePrac;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
